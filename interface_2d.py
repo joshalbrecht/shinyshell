d2ac41e8a616032382c47395db90497986616423
Josh Albrecht <thejash@gmail.com>
2014-12-31 12:47:52 -0800

    broke shell into two objects so they can be selected and moved appropriately

--- interface_2d.py
+++ interface_2d.py
@@ -33,10 +33,55 @@ class Ray(LineSegment):
     def __init__(self, start, end):
         LineSegment.__init__(self, start, end, color=(0.5, 0.5, 0.5))
 
+class SceneObject(object):
+    """
+    :attr pos: the position of the object in real coordinates (mm)
+    :attr change_handler: will be called if the position changes
+    """
+    
+    def __init__(self, pos=None, color=(1.0, 1.0, 1.0), change_handler=None):
+        assert pos != None, "Must define a position for a SceneObject"
+        self._pos = pos
+        self._color = color
+        self._change_handler = change_handler
+        
+    def on_change(self):
+        if self._change_handler:
+            self._change_handler()
+            
+    def render(self):
+        glColor3f(*self._color)
+        
+    @property
+    def pos(self): 
+        return self._pos
+
+    @pos.setter
+    def pos(self, value): 
+        self._pos = value
+        self.on_change()
+
+class ScreenPixel(SceneObject):
+    def render(self):
+        SceneObject.render(self)
+        glBegin(GL_POINTS)
+        glVertex3f(*self._pos)
+        glEnd()
+
+class ReflectiveSurface(SceneObject):
+    def __init__(self, **kwargs):
+        SceneObject.__init__(self, **kwargs)
+        self.segments = []
+        
+    def render(self):
+        SceneObject.render(self)
+        for segment in self.segments:
+            segment.render()
+        
 class ShellSection(object):
     """
-    :attr shell_pos: the position of the center of the shell (in real coordinates, mm)
-    :attr pixel_pos: the position of the pixel on the screen (in real coordinates, mm)
+    :attr shell: the object representing the center of the shell
+    :attr pixel: the object representing the pixel on the screen
     :attr num_rays: the number of parallel rays emitted by the eye
     :attr pupil_radius: 1/2 of the width of the region that emits rays from the eye
     
@@ -44,70 +89,43 @@ class ShellSection(object):
     """
     
     def __init__(self, shell_pos, pixel_pos, num_rays, pupil_radius):
-        self._shell_pos = shell_pos
-        self._pixel_pos = pixel_pos
+        self._shell = ReflectiveSurface(pos=shell_pos, color=(1.0, 1.0, 1.0), change_handler=self._recalculate)
+        self._pixel = ScreenPixel(pos=pixel_pos, color=(1.0, 1.0, 1.0), change_handler=self._recalculate)
         self._num_rays = num_rays
         self._pupil_radius = pupil_radius
         
         #calculated attributes
         self._rays = []
-        self._segments = []
         
         self._recalculate()
         
     def render(self):
         """Draw the shell section, pixel, and rays"""
-        #draw the raws
+        self._shell.render()
+        self._pixel.render()
         for ray in self._rays:
             ray.render()
             
-        #draw the segments
-        for segment in self._segments:
-            segment.render()
-        
-        #draw the pixel
-        glBegin(GL_POINTS)
-        glVertex3f(*self.pixel_pos)
-        glEnd()
-        
     def _recalculate(self):
         """Update internal state when any of the interesting variables have changed"""
         
         #note: this is just a hacky implementation right now to see if things are generally working
-        point_to_eye = _normalize(Point3D(0,0,0) - self._shell_pos)
-        point_to_pixel = _normalize(self._pixel_pos - self._shell_pos)
+        point_to_eye = _normalize(Point3D(0,0,0) - self._shell.pos)
+        point_to_pixel = _normalize(self._pixel.pos - self._shell.pos)
         surface_normal = _normalize((point_to_eye + point_to_pixel) / 2.0)
         tangent = Point3D(0.0, -1.0 * surface_normal[2], surface_normal[1])
-        start = self._shell_pos + tangent
-        end = self._shell_pos - tangent
+        start = self._shell.pos + tangent
+        end = self._shell.pos - tangent
         segment = LineSegment(start, end)
-        self._segments = [segment]
-        self._rays = [Ray(Point3D(0,0,0), self._shell_pos), Ray(self._shell_pos, self._pixel_pos)]
+        self._shell.segments = [segment]
+        self._rays = [Ray(Point3D(0,0,0), self._shell.pos), Ray(self._shell.pos, self._pixel.pos)]
         
         #TODO: do the right thing
         #figure out the angle of the primary ray
         #define a vector field for the surface normals of the shell. They are completely constrained given the location of the pixel and the fact that the reflecting ray must be at a particular angle
         #use that vector field to define the exact shape of the surface
         #simply pre-create a list of rays and line segments for the shell (all to be rendered later)
-        
-    @property
-    def shell_pos(self): 
-        return self._shell_pos
 
-    @shell_pos.setter
-    def shell_pos(self, value): 
-        self._shell_pos = value
-        self._recalculate()
-        
-    @property
-    def pixel_pos(self): 
-        return self._pixel_pos
-
-    @pixel_pos.setter
-    def pixel_pos(self, value): 
-        self._pixel_pos = value
-        self._recalculate()
-        
     @property
     def num_rays(self): 
         return self._num_rays
