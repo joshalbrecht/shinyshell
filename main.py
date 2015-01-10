minor refactorings and TODO fixing and a bug fix (was not exiting after threads were done for next calculation)

main.py
@@ -134,7 +134,7 @@ from pyoptools.misc import cmisc
 from pyoptools.misc import pmisc
 import scipy.integrate
 
-from optics.base import Point3D, Point2D, normalize, normalized_vector_angle, get_arc_plane_normal, angle_vector_to_vector, AngleVector, create_transform_matrix_from_rotations
+from optics.base import Point3D, Point2D, normalize, normalized_vector_angle, get_arc_plane_normal, angle_vector_to_vector, AngleVector, _create_transform_matrix_from_rotations
 import optics.globals
 import optics.rotation_matrix
 import optics.mesh
@@ -167,7 +167,7 @@ class Screen(object):
     :attr position: the location of the center of the screen
     :type position: Point3D
     :attr rotations: (rx,ry,yz). Pretty dumb way of thinking about things but whatever.
-    see 'create_transform_matrix_from_rotations' for a description of the format
+    see '_create_transform_matrix_from_rotations' for a description of the format
     :type rotations: Point3D
     :attr size: the width and height of the screen
     :type size: Point2D
@@ -181,7 +181,7 @@ class Screen(object):
         self.size = size
         self.pixel_distribution = pixel_distribution
 
-        rot_mat = create_transform_matrix_from_rotations(self.rotations)
+        rot_mat = _create_transform_matrix_from_rotations(self.rotations)
 
         self.side_vector = rot_mat.dot(Point3D(1.0, 0.0, 0.0))
         self.up_vector = rot_mat.dot(Point3D(0.0, 1.0, 0.0))
@@ -293,7 +293,7 @@ def create_rays_from_screen(screen, fov):
             rotations.append((xangle, yangle, 0))
 
     for rot in rotations:
-        rot_mat = create_transform_matrix_from_rotations(rot)
+        rot_mat = _create_transform_matrix_from_rotations(rot)
         rays.append(Ray(screen.vision_ray_to_pixel(AngleVector(theta, phi)), dir=rot_mat.dot(screen.direction)))
 
     return rays
@@ -560,7 +560,7 @@ def create_rays_from_screen_center_2d(screen, fov):
         rotations.append((angle, 0, 0))
 
     for rot in rotations:
-        rot_mat = create_transform_matrix_from_rotations(rot)
+        rot_mat = _create_transform_matrix_from_rotations(rot)
         rays.append(Ray(screen.vision_ray_to_pixel(AngleVector(theta, phi)), dir=rot_mat.dot(screen.direction)))
     return rays
 
@@ -582,7 +582,7 @@ def create_rays_from_multiple_pixels_on_screen_2d(screen, fov):
 
     for theta, phi in pixel_list:
         for rot in rotations:
-            rot_mat = create_transform_matrix_from_rotations(rot)
+            rot_mat = __create_transform_matrix_from_rotations(rot)
             rays.append(Ray(screen.vision_ray_to_pixel(AngleVector(theta, phi)), dir=rot_mat.dot(screen.direction)))
     return rays
 

optics/calculations.py
@@ -44,28 +44,9 @@ def create_arc_helper(shell_point, screen_point, light_radius, arc_plane_normal,
         derivative = normalize(numpy.cross(surface_normal, arc_plane_normal))
         return derivative
     
-    #TODO: probably worth doing finer steps along a smaller number of rungs so that they integrate better
-    
-    #TODO: this should really be based on light_radius...
-    
-    #estimate how long the piece of the shell will be (the one that is large enough to reflect all rays)
-    #overestimates will waste time, underestimates cause it to crash :-P
-    #note that we're doing this one half at a time
-    def estimate_t_values():
-        #TODO: make this faster if necessary by doing the following:
-            #define the simple line that reflects the primary ray
-            #intersect that with the max and min rays from the eye
-            #check the distance between those intersections and double it or something
-        if optics.globals.QUALITY_MODE == optics.globals.ULTRA_LOW_QUALITY_MODE:
-            t_step = 0.1
-        elif optics.globals.QUALITY_MODE == optics.globals.LOW_QUALITY_MODE:
-            t_step = 0.1
-        else:
-            t_step = 0.1
-        max_t = 5.0
-        return numpy.arange(0.0, max_t, t_step)
-    t_values = estimate_t_values()
-    every_nth = 10
+    t_step = 0.1
+    max_t = 1.1 * light_radius
+    t_values = numpy.arange(0.0, max_t, t_step)
 
     #use the vector field to define the exact shape of the surface (first half)
     half_arc = scipy.integrate.odeint(f, shell_point, t_values)
@@ -74,10 +55,8 @@ def create_arc_helper(shell_point, screen_point, light_radius, arc_plane_normal,
     def g(point, t):
         return -1.0 * f(point, t)
     
-    if optics.globals.QUALITY_MODE != optics.globals.HIGH_QUALITY_MODE:
-        numpy.concatenate((scipy.integrate.odeint(g, shell_point, t_values)[1:][::-1], half_arc))
-        
     #combine them
+    every_nth = 10
     return numpy.concatenate((scipy.integrate.odeint(g, shell_point, t_values)[::every_nth][1:][::-1], half_arc[::every_nth]))
 
 def polyfit2d(x, y, z, order=3):
@@ -166,7 +145,7 @@ def calculate_error(scale, reference_scale, best_error_so_far):
     points = reference_scale.points()
     for point in points:
         end = 2.0 * (point - start) + start
-        intersection_point, intersection_normal = scale.intersection_plus_normal(start, end)
+        intersection_point = scale.intersection(start, end)
         if intersection_point != None:
             num_hits += 1
             #print numpy.linalg.norm(intersection_point - point)
@@ -373,7 +352,7 @@ def create_surface_via_scales(initial_shell_point, initial_screen_point, screen_
     upper_bound = 2.0 * light_radius
         
     phi_step = 0.05
-    final_phi = fov/2.0
+    final_phi = 0.000001#fov/2.0
     
     #this is side to side motion
     lateral_normal = normalize(numpy.cross(principal_ray, screen_normal))
@@ -413,7 +392,9 @@ def create_surface_via_scales(initial_shell_point, initial_screen_point, screen_
         thread.start()
         
     while True:
-        all_threads_done = [not t.is_alive() for t in threads]
+        all_threads_done = True not in [t.is_alive() for t in threads]
+        if all_threads_done:
+            break
     
         if stop_flag.is_set():
             return []